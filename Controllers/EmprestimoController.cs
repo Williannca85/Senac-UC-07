3b3d6c8216bfb7f53e6d3b8fec842911aec87e1d
Williannca85 <williannca@hotmail.com>
2021-11-04 22:34:39 -0300

    Listagem de emprestimos com a informação de somente livros disponiveis

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -11,54 +11,80 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+            Autenticacao.CheckLogin(this);/* adicionado da video aula */
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+           
+            cadModel.Livros = livroService.ListarDisponiveis();/* anteriormente cadModel.Livros = livroService.ListarTodos(); */
             return View(cadModel);
         }
 
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
-        {
-            EmprestimoService emprestimoService = new EmprestimoService();
+     
+       {
+            if(!string.IsNullOrEmpty(viewModel.Emprestimo.NomeUsuario) ) /* adicionado da video aula */
+            {   
+
+                    EmprestimoService emprestimoService = new EmprestimoService();
             
-            if(viewModel.Emprestimo.Id == 0)
-            {
-                emprestimoService.Inserir(viewModel.Emprestimo);
-            }
-            else
-            {
-                emprestimoService.Atualizar(viewModel.Emprestimo);
-            }
-            return RedirectToAction("Listagem");
-        }
+                    if(viewModel.Emprestimo.Id == 0)
+                    {
+                        emprestimoService.Inserir(viewModel.Emprestimo);
+                    }
+                    else
+                    {
+                        emprestimoService.Atualizar(viewModel.Emprestimo);
+                    }
 
-        public IActionResult Listagem(string tipoFiltro, string filtro)
-        {
-            FiltrosEmprestimos objFiltro = null;
-            if(!string.IsNullOrEmpty(filtro))
+                    return RedirectToAction("Listagem");
+                }
+                else
+                {       
+                    ViewData["mensagem"] = "Por favor, Preencha todos os campos";  
+
+                    LivroService livroService = new LivroService();
+                    EmprestimoService emprestimoService = new EmprestimoService();
+
+                    CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+
+                    cadModel.Livros = livroService.ListarDisponiveis();
+
+                    return View(cadModel);
+                }
+        }   
+
+            public IActionResult Listagem(string tipoFiltro, string filtro)
             {
-                objFiltro = new FiltrosEmprestimos();
-                objFiltro.Filtro = filtro;
-                objFiltro.TipoFiltro = tipoFiltro;
+                Autenticacao.CheckLogin(this);/* adicionado da video aula */
+                FiltrosEmprestimos objFiltro = null;
+
+                if(!string.IsNullOrEmpty(filtro))
+                {
+                    objFiltro = new FiltrosEmprestimos();
+                    objFiltro.Filtro = filtro;
+                    objFiltro.TipoFiltro = tipoFiltro;
+                }
+
+                EmprestimoService emprestimoService = new EmprestimoService();
+                return View(emprestimoService.ListarTodos(objFiltro));
             }
-            EmprestimoService emprestimoService = new EmprestimoService();
-            return View(emprestimoService.ListarTodos(objFiltro));
-        }
 
-        public IActionResult Edicao(int id)
-        {
-            LivroService livroService = new LivroService();
-            EmprestimoService em = new EmprestimoService();
-            Emprestimo e = em.ObterPorId(id);
+            public IActionResult Edicao(int id)
+            {
+                Autenticacao.CheckLogin(this);/* adicionado da video aula */
+                LivroService livroService = new LivroService();
+                EmprestimoService em = new EmprestimoService();
+                Emprestimo e = em.ObterPorId(id);
 
-            CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
-            cadModel.Emprestimo = e;
-            
-            return View(cadModel);
-        }
-    }
+                CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+                cadModel.Livros = livroService.ListarDisponiveis(); /* anteriormente cadModel.Livros = livroService.ListarTodos(); */
+                cadModel.Livros.Add(livroService.ObterPorId(e.LivroId)); /* adicionado da video aula */
+                cadModel.Emprestimo = e;
+                
+                return View(cadModel);
+            }
+     }
 }
